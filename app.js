075272d07ed0f5036e296362ed0e773d08384549
sshanahan91 <stephenshanahan91@gmail.com>
2015-12-29 21:27:44 -0800

    set up Mongoose.js database connection to MongoDB

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
-
+var db = require('./db');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
